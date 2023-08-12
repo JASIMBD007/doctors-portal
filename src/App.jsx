0fa2d63dd10aa91aceaf18d6a8a3862ee06862c4
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-08-12 19:04:22 +0200

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css'
 import Navbar from './Pages/Shared/Navbar/Navbar'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import About from './Pages/About/About'
 import Login from './Pages/Login/Login'
-import Appointment from './Pages/Appointment/Appointment'
-import Reviews from './Pages/Reviews/Reviews'
-import Contact from './Pages/Contact/Contact'
 
 function App () {
 
@@ -16,9 +13,6 @@ function App () {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/appointment" element={<Appointment />} />
-        <Route path="/reviews" element={<Reviews />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
       </Routes>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import Treatment from './Treatment'
 import MakeAppointment from './MakeAppointment'
 import Testimonials from './Testimonials'
 import Contact from './Contact'
+import Footer from './Footer'
 
 const Home = () => {
     return (
@@ -17,6 +18,7 @@ const Home = () => {
             <MakeAppointment />
             <Testimonials />
             <Contact />
+            <Footer />
         </div>
     )
 }
