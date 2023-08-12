5a8194ceda0dd9cfb06200a35fe92d8ec6ecd972
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-08-13 00:34:10 +0200

    responsive small device issue fixed

--- src/Pages/Home/Info.jsx
+++ src/Pages/Home/Info.jsx
@@ -8,9 +8,9 @@ import phone from '../../assets/icons/phone.svg'
 const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Opening Hours" img={clock} />
-            <InfoCard bgClass="bg-accent" cardTitle="Visit Our Location" img={marker} />
-            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Contact Us Now" img={phone} />
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Opening Hours" img={clock} info="Lorem Ipsum is simply dummy text of the pri" />
+            <InfoCard bgClass="bg-accent" cardTitle="Visit Our Location" img={marker} info="Brooklyn, NY 10036, United States" />
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Contact Us Now" img={phone} info="+000 123 456789" />
         </div>
     )
 }
