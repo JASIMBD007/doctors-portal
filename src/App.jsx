14a1f838f81d336a5cc4560df7bbc84899a2447b
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-08-12 13:12:48 +0200

    components and routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './Pages/Shared/Navbar/Navbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Login from './Pages/Login/Login'
+import Appointment from './Pages/Appointment/Appointment'
+import Reviews from './Pages/Reviews/Reviews'
+import Contact from './Pages/Contact/Contact'
 
 function App () {
 
@@ -8,6 +14,15 @@ function App () {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
     </>
   )
 }
