19f9b6ec940a7c2f369dccda5d733b92ccf0260e
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-08-13 23:41:14 +0200

    some issue with text position in banner fixed

--- src/Pages/Appointment/AppointmentBanner.jsx
+++ src/Pages/Appointment/AppointmentBanner.jsx
@@ -1,24 +1,25 @@
-import chair from '../../assets/images/chair.png';
+import React from 'react';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
-import bg from '../../assets/images/bg.png'
-
+import chair from '../../assets/images/chair.png';
+import bg from '../../assets/images/bg.png';
 
 const AppointmentBanner = ({ date, setDate }) => {
     return (
-        <div style={{ background: `url(${bg})` }} className="hero min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
+        <div style={{ background: `url(${bg})` }} className="hero min-h-screen flex flex-col justify-center items-center">
+            <h2 className="text-3xl  font-bold mb-24 text-center text-secondary">Book Your Appointment Here!</h2>
+            <div className="flex flex-col lg:flex-row items-center">
+                <img src={chair} alt="Chair" className="max-w-sm rounded-lg shadow-2xl lg:order-2 ml-16" />
+                <div className="lg:order-1 lg:mr-16 ">
                     <DayPicker
                         mode="single"
                         selected={date}
                         onSelect={setDate}
                     />
-
                 </div>
             </div>
-        </div>)
-}
+        </div>
+    );
+};
 
-export default AppointmentBanner
+export default AppointmentBanner;
