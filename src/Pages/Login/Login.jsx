b6b9e891bfa22047b4ddd2725ec6c8d5c4fe9bbb
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-08-18 16:10:59 +0200

    react hook form validation and regex added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,17 +1,92 @@
 import React from 'react'
 import auth from '../../firebase.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const { register, formState: { errors }, handleSubmit, } = useForm();
+
     if (user) {
         console.log(user);
     }
+
+    const onSubmit = (data) => console.log(data)
+
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="text-center text-2xl font-bold">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Your Email"
+                                className="input input-bordered w-full max-w-xs" {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is Required'
+                                    },
+                                    pattern: {
+                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                        message: 'Provide a valid Email' // JS only: <p>error message</p> TS only support string
+                                    }
+                                })}
+
+                            />
+                            <label className="label">
+                                {errors.email?.type === "required" && (
+                                    <p role="alert">{errors.email.message}</p>
+                                )}
+                                <span className="label-text-alt">Bottom Left label</span>
+                            </label>
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className="input input-bordered w-full max-w-xs" {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is Required'
+                                    },
+                                    pattern: {
+                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                        message: 'Provide a valid Email' // JS only: <p>error message</p> TS only support string
+                                    }
+                                })}
+
+                            />
+                            <label className="label">
+                                {errors.email?.type === "required" && (
+                                    <p role="alert">{errors.email.message}</p>
+                                )}
+                                <span className="label-text-alt">Bottom Left label</span>
+                            </label>
+                        </div>
+                        <input
+
+                            aria-invalid={errors.firstName ? "true" : "false"}
+                        />
+                        {errors.firstName?.type === "required" && (
+                            <p role="alert">First name is required</p>
+                        )}
+
+                        <input
+                            {...register("mail", { required: "Email Address is required" })}
+                            aria-invalid={errors.mail ? "true" : "false"}
+                        />
+                        {errors.mail && <p role="alert">{errors.mail.message}</p>}
+
+                        <input type="submit" />
+                    </form>
                     <div className="divider">OR</div>
                     <button onClick={() => signInWithGoogle()} className="btn btn-outline">CONTINUE WITH GOOGLE</button>
                 </div>
