0a5113fe4933be313e73552e969865bb797e4716
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-08-20 19:37:16 +0200

    dashboard and data showed as a table

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -4,15 +4,14 @@ import { Link, Outlet } from 'react-router-dom'
 const Dashboard = () => {
     return (
         <div className="drawer lg:drawer-open">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
-                <h2 className='text-5xl' >Dashboard</h2>
+            <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+                <h2 className='text-3xl font-bold' >Welcome To Your Dashboard</h2>
                 <Outlet />
-                <label htmlFor="my-drawer-2" className="btn btn-secondary drawer-button lg:hidden">Dashboard</label>
 
             </div>
             <div className="drawer-side">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
                     {/* Sidebar content here */}
                     <li><Link to='/dashboard'>Dashboard</Link></li>
